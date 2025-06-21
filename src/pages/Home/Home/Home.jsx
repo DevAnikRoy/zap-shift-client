2659c09efd38a5f7be82cfe461df44719358d7db
DevAnikRoy <anikroy302@gmail.com>
2025-06-22 00:30:12 +0600

    first commit

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>This is the Home page Of the Zap-Shift</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Home/Home/shared/ZapShiftLogo/ZapShiftLogo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../../../../assets/logo.png'
+
+const ZapShiftLogo = () => {
+    return (
+        <div>
+            <img src={logo} alt="" />
+            <p>Zap-Shift</p>
+        </div>
+    );
+};
+
+export default ZapShiftLogo;
